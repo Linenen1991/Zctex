627e284476d817e6df46b980b76a696041b812ca
Bojing <binfan@visionatics.com.tw>
2026-03-22 21:48:21 +0800

    init, wait rename

---
+++ MySampleProject.Tests/FilterTests.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using MySampleProject;
+
+namespace MySampleProject.Tests
+{
+    public class FilterTests
+    {
+
+    }
+}
---
+++ MySampleProject/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MySampleProject
+{
+    internal class Program
+    {
+        internal static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
