ac1984f9c8ce9a31407efc9c4453f7ae5a24f8d4
Bojing <binfan@visionatics.com.tw>
2026-04-07 00:57:46 +0800

    winform

--- AsyncElastic/Program.cs
+++ AsyncElastic/Program.cs
@@ -1,14 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-
 namespace AsyncElastic
 {
-    internal class Program
+    internal static class Program
     {
-        internal static int Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            return 0;
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
         }
     }
 }
