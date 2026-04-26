cad24b9224d7836aa26634d32fc4b31b7ffc9254
Bojing <binfan@visionatics.com.tw>
2026-04-26 23:49:58 +0800

    Init

---
+++ WPF_GUI/DataCenter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_GUI
+{
+    public class LogsData
+    {
+        private string _serviceName;
+        private string _className;
+        private string _methodName;
+        private string _blockBelong;
+        private DateTime _timestamp;
+
+        public string ServiceName { get => _serviceName; set => _serviceName = value; }
+        public string ClassName { get => _className; set => _className = value; }
+        public string MethodName { get => _methodName; set => _methodName = value; }
+        public string BlockBelong { get => _blockBelong; set => _blockBelong = value; }
+        public DateTime Timestamp { get => _timestamp; set => _timestamp = value; }
+    }
+    internal class DataCenter
+    {
+        public List<LogsData> GetPage(int pageIndex)
+        {
+            return new List<LogsData>();
+        }
+
+        public List<LogsData> GetFileted(int pageIndex)
+        {
+            return new List<LogsData>();
+        }
+
+        public List<LogsData> GetEventCodeOnly()
+        {
+            return new List<LogsData>();
+        }
+    }
+}
--- WPF_GUI/Properties/AssemblyInfo.cs
+++ WPF_GUI/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
-using System.Reflection;
+﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
@@ -19,8 +21,25 @@ using System.Runtime.InteropServices;
 // 的類型，請在該類型上將 ComVisible 屬性設定為 true。
 [assembly: ComVisible(false)]
 
-// 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("d3da63d2-f215-4cd4-8af9-c5e6357f308d")]
+//若要開始建置可當地語系化的應用程式，請在
+//.csproj 檔案中的 <UICulture>CultureYouAreCodingWith</UICulture>
+//在 <PropertyGroup> 中。例如，如果原始程式檔使用美式英文， 
+//請將 <UICulture> 設為 en-US。然後取消註解下列
+//NeutralResourceLanguage 屬性。在下一行中更新 "en-US"，
+//以符合專案檔中的 UICulture 設定。
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //主題特定資源字典的位置
+                                     //(在頁面中找不到時使用，
+                                     // 或應用程式資源字典中找不到資源時)
+    ResourceDictionaryLocation.SourceAssembly //泛型資源字典的位置
+                                              //(在頁面中找不到時使用，
+                                              // 或是應用程式或任何主題特定資源字典中找不到資源時)
+)]
+
 
 // 組件的版本資訊由下列四個值所組成:
 //
@@ -34,4 +53,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("WPF_GUI.Tests")]
